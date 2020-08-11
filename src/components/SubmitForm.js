29fbc2b1783f300dad58cf2905dd0d227964c6c9
Alex <alex.oommen@softvision.com>
2020-08-12 03:21:40 +0530

    Added agree button

--- src/components/SubmitForm.js
+++ src/components/SubmitForm.js
@@ -2,31 +2,38 @@ import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
 
-function SubmitForm() {    
-    let selector = useSelector(state =>  state.formData);
-    const navigate = useHistory();    
-            if(!selector.name) {
-                navigate.goBack();
-            }
+function SubmitForm() {
+    let selector = useSelector(state => state.formData);
+    const navigate = useHistory();
+    if (!selector.name) {
+        navigate.goBack();
+    }
 
     return (
         <div className="container d-flex justify-content-center align-items-center min-vh-100">
             <div className="row">
                 <div className="col-sm-3">
-                { selector.image &&   
-                        <div>                      
-                        <div className="form-group">
-                            <img src={selector.image} alt='' height="150px" width="150px"/>
-                        </div>
-                        {/* <input type="file" accept="image/*" className="form-control" placeholder="Upload your photo" onChange={uploadSingleFile} />
+                    {selector.image &&
+                        <div>
+                            <div className="form-group">
+                                <img src={selector.image} alt='' height="150px" width="150px" />
+                            </div>
+                            {/* <input type="file" accept="image/*" className="form-control" placeholder="Upload your photo" onChange={uploadSingleFile} />
                         <button onClick={uploadPhoto} className="btn btn-link">Change Photo</button>                            */}
-                        </div> 
-                }
+                        </div>
+                    }
                 </div>
                 <div className="col-sm-9">
                     I am {selector.name.firstName} {selector.name.secondName} and I am above {selector.age} years and you can send your emails to {selector.email},
                     I lives in the state of {selector.state} .I likes to play hockey, football and ,t'ad'ilP baCk,.
                     {selector.subscribe && <span>And please send me the news letters.</span>} Please reach out to me on my phone {selector.telephone}.
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="text-center mt-5">
+                                <button className="btn btn-primary btn-lg" type="button">Agree</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
