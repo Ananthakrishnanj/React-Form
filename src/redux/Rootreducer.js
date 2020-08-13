d40026383c425ed24ab312d4697db7d9c0bbf79c
Alex <alex.oommen@softvision.com>
2020-08-13 22:11:46 +0530

    Removed console log

--- src/redux/Rootreducer.js
+++ src/redux/Rootreducer.js
@@ -2,8 +2,7 @@ const initialState = {
     formData: undefined
 }
 
-export default function rootReducer(state = initialState, action) {
-    console.log("Action data : ", action);
+export default function rootReducer(state = initialState, action) {    
     switch(action.type) {
         case "UPDATE_FORM_DATA":                         
             return {...state, formData: action.payload};            
